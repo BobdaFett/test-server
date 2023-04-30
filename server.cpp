ddfe3cdc5d0921fc236b8809c10ef08fba2991a3
BobdaFett <lvas463@gmail.com>
2023-04-29 22:14:06 -0400

    Added a couple extremely simple commands.

--- server.cpp
+++ server.cpp
@@ -1,9 +1,13 @@
 #using "system.dll"
 
 using namespace System;
+using namespace System::Text;
 using namespace System::IO;
 using namespace System::Net;
 using namespace System::Net::Sockets;
+
+int Add(int, int);
+int Subtract(int, int);
 
 int main(array<String^>^ args) {
 	int port = 2345;
@@ -30,19 +34,39 @@ int main(array<String^>^ args) {
 
 	// Only accepts a single connection, then exits.
 	try {
-		int value = 0;
+		String^ value = "";
 
-		// Run this while the server is getting input. A specific error will end the stream.
+		// Run this while the server is getting input. An EndOfStreamException will exit this loop.
+		// Could rearrange this to accept multiple different connections, and create some sort of "sessions."
 		while (true) {
 			Console::WriteLine("Attempting to read data.");
-			value = reader->ReadInt32();
+			value = reader->ReadString();
+			Console::WriteLine("Data recieved was \"{0}\"", value);
 
-			Console::WriteLine("Data recieved was {0}", value);
-			Console::Write("Sending {0} + 100... ", value);
+			array<String^>^ commands = value->Split(' ');
+			int endValue = 0;
+			if (commands[0] == "ADD") {
+				Console::WriteLine("Adding values.");
+				// Get values from the command string.
+				int num1 = Int32::Parse(commands[1]);
+				int num2 = Int32::Parse(commands[2]);
+				endValue = Add(num1, num2);
+			}
+			else if (commands[0] == "SUBTRACT") {
+				Console::WriteLine("Subtracting values.");
+				// Get values from the command string.
+				int num1 = Int32::Parse(commands[1]);
+				int num2 = Int32::Parse(commands[2]);
+				endValue = Subtract(num1, num2);
+			}
+			else {
+				Console::WriteLine("Command not found.");
+			}
 
-			value += 100;
-			writer->Write(value);
-			Console::WriteLine("Sent.");
+			Console::Write("Sending response... ", value);
+			String^ response = "Recieved.";
+			writer->Write(endValue);
+			Console::WriteLine("Sent {0}", endValue);
 		}
 	}
 	catch (EndOfStreamException^ e) {
@@ -61,4 +85,14 @@ int main(array<String^>^ args) {
 	Console::WriteLine("Complete.");
 	Console::Write("\nPress any key to exit...");
 	Console::ReadKey();
+}
+
+int Add(int a, int b) {
+	// Add values and return new value.
+	return a + b;
+}
+
+int Subtract(int a, int b) {
+	// Subtract values and return new value.
+	return a - b;
 }
